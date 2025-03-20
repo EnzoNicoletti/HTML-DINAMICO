056c155582048e22dcf5388121a4d3cfb849c36f
EnzoNicoletti <enzo.nicoletti@aluno.senai.br>
2025-03-20 16:10:47 -0300

    Iniciando repositorio

---
+++ 03-Lampada/js/script.js
@@ -0,0 +1,5 @@
+var lampada = document.getElementById("lampada")
+
+const luzSimples = (cor) =>{
+    lampada.style.backgroundColor = cor
+}
---
+++ 04-FrasesAleatorias/js/script.js
@@ -0,0 +1,28 @@
+// Criando a função de flecha com frases aleatórias
+const falar = () => {
+  // Criando o vetor de frases
+  const frases = [
+    "Hoje está um dia bonito!",
+    "Falta muito pra sexta-feira?",
+    "Qual o cardápio de hoje no almoço?",
+    "Vish, tem prova de matemática!!!",
+  ];
+
+  //Criando uma variável para armazenar a DIV com id "tagarela"
+  let tagarela = document.getElementById("tagarela");
+
+  //Criando variável de controle para a frase exibida
+  let indice = 0;
+
+  //Criando variável para número aleatório entre 0 e 1 (lembrando que o 1 não entra na conta)
+  let num = Math.random();
+
+  //Configurando o número para a frase selecionada
+  if (num > 0.75) indice = 3;
+  else if (num > 0.5) indice = 2;
+  else if (num > 0.25) indice = 1;
+  else indice = 0;
+
+  //Escrevendo a frase para o usuário
+  tagarela.innerHTML = frases[indice];
+};
